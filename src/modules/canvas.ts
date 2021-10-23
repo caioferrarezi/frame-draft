24687532c83a363363cbf836d0696884d6afe99b
Caio Ferrarezi <caioferrarezi2@gmail.com>
2021-10-23 15:21:45 -0300

    simplify the canvas class

--- src/modules/canvas.ts
+++ src/modules/canvas.ts
@@ -1,21 +1,21 @@
 export class Canvas {
+  private canvas: HTMLCanvasElement
   public context: CanvasRenderingContext2D
-  private display: CanvasRenderingContext2D
 
   constructor(width: number, height: number) {
     const deviceRatio = window.devicePixelRatio || 1
 
-    this.context = document.createElement('canvas').getContext('2d')
-    this.display = document.createElement('canvas').getContext('2d')
+    this.canvas = document.createElement('canvas')
+    this.context = this.canvas.getContext('2d')
 
-    this.context.canvas.width = width * deviceRatio
-    this.context.canvas.height = height * deviceRatio
+    this.canvas.width = width * deviceRatio
+    this.canvas.height = height * deviceRatio
 
     this.context.scale(deviceRatio, deviceRatio)
 
     this.resize()
 
-    document.body.insertAdjacentElement('afterbegin', this.display.canvas)
+    document.body.insertAdjacentElement('afterbegin', this.canvas)
 
     window.addEventListener('resize', () => this.resize())
   }
@@ -24,22 +24,14 @@ export class Canvas {
     const width = document.body.clientWidth
     const height = document.body.clientHeight
 
-    const aspectRatio = this.context.canvas.height / this.context.canvas.width
+    const aspectRatio = this.canvas.height / this.canvas.width
 
     if (aspectRatio < height / width) {
-      this.display.canvas.width = width
-      this.display.canvas.height = width * aspectRatio
+      this.canvas.style.width = `${width}px`
+      this.canvas.style.height = `${width * aspectRatio}px`
     } else {
-      this.display.canvas.width = height / aspectRatio
-      this.display.canvas.height = height
+      this.canvas.style.width = `${height/ aspectRatio}px`
+      this.canvas.style.height = `${height}px`
     }
-  }
-
-  public render() {
-    this.display.drawImage(
-      this.context.canvas,
-      0, 0, this.context.canvas.width, this.context.canvas.height,
-      0, 0, this.display.canvas.width, this.display.canvas.height
-    )
   }
 }
